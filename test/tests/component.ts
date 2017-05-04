443df71768643803765ef3fb4c196bc11051634c
strantr <strantr@users.noreply.github.com>
2017-05-04 19:43:50 +0100

    Added test for default names Closes #16

--- test/tests/component.ts
+++ test/tests/component.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { Component } from "../../src";
 import Vue from "vue";
 
-export default function() {
+export default function () {
     describe("Component", () => {
         it("Should create new Vue element", () => {
             @Component() class A extends Vue {
@@ -17,6 +17,25 @@ export default function() {
             }) class A extends Vue {
             }
             expect(new A().$options.template).to.equal(template);
+        });
+
+        it("Allows a name to be provided", () => {
+            const template = "<div>a</div>";
+            @Component({
+                template,
+                name: "hello"
+            }) class Test extends Vue {
+            }
+            expect(new Test().$options.name).to.equal("hello");
+        });
+
+        it("Provides a name if not provided", () => {
+            const template = "<div>a</div>";
+            @Component({
+                template
+            }) class Test extends Vue {
+            }
+            expect(new Test().$options.name).to.equal("Test");
         });
     });
 }
