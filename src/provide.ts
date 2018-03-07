961bc2fb60e567233e6fcf056d550f480bb461ab
strantr <strantr@users.noreply.github.com>
2018-03-07 19:49:17 +0000

    Added fix for vue warn

--- src/provide.ts
+++ src/provide.ts
@@ -1,32 +1,38 @@
 import * as Vue from "vue";
 import { IVuety, Vuety, DecoratorFactory } from "./core";
 
-export function Provide(target: Vue, propertyKey: string): any
+export function Provide(target: Vue, propertyKey: string): any;
 export function Provide(dependencyName?: string): DecoratorFactory<string>;
 export function Provide(this: Vue): DecoratorFactory<string> | undefined {
-    function provide(dependencyName: string | undefined, target: IVuety, propertyKey: string, descriptor: PropertyDescriptor) {
-        Vuety("Provide", target)(v => {
-            const provided: { [k: string]: string } = v.proto["$vuety-provide"] = v.proto["$vuety-provide"] || {};
-            provided[dependencyName || propertyKey] = propertyKey;
+	function provide(
+		dependencyName: string | undefined,
+		target: IVuety,
+		propertyKey: string,
+		descriptor: PropertyDescriptor
+	) {
+		Vuety("Provide", target)(v => {
+			const provided: { [k: string]: string } = (v.proto["$vuety-provide"] = v.proto["$vuety-provide"] || {});
+			provided[dependencyName || propertyKey] = propertyKey;
 
-            v.done(data => {
-                data.options.provide = function (this: Vue) {
-                    let provide = {};
-                    Object.keys(provided).forEach(k => provide[k] = this[provided[k]]);
-                    return provide;
-                }
-            });
-        });
-    }
+			v.done(data => {
+				delete v.proto["$vuety-provide"];
+				data.options.provide = function(this: Vue) {
+					let provide = {};
+					Object.keys(provided).forEach(k => (provide[k] = this[provided[k]]));
+					return provide;
+				};
+			});
+		});
+	}
 
-    let args = Array.from(arguments);
-    if (args[0] instanceof Vue) {
-        provide.apply(this, [undefined, ...args]);
-        return;
-    } else {
-        args.length = 1;
-        return function (this: IVuety) {
-            provide.apply(this, [...args, ...Array.from(arguments)]);
-        };
-    }
+	let args = Array.from(arguments);
+	if (args[0] instanceof Vue) {
+		provide.apply(this, [undefined, ...args]);
+		return;
+	} else {
+		args.length = 1;
+		return function(this: IVuety) {
+			provide.apply(this, [...args, ...Array.from(arguments)]);
+		};
+	}
 }
