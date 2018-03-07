c7f1fc87419783755242fdb07d58b48b5fb090c4
strantr <strantr@users.noreply.github.com>
2018-03-07 19:45:35 +0000

    Added support for vue default inject value

--- src/inject.ts
+++ src/inject.ts
@@ -1,27 +1,35 @@
 import * as Vue from "vue";
 import { IVuety, Vuety, DecoratorFactory } from "./core";
 
-export function Inject(target: Vue, propertyKey: string): any
-export function Inject(dependencyName?: string): DecoratorFactory<string>;
-export function Inject(this: Vue): DecoratorFactory<string> | undefined {
-    function inject(dependencyName: string | undefined, target: IVuety, propertyKey: string, descriptor: PropertyDescriptor) {
-        Vuety("Inject", target)(v => {
-            if (!v.options.inject) {
-                v.options.inject = {};
-            }
-            (v.options.inject as {})[propertyKey] = dependencyName || propertyKey;
-            delete v.proto[propertyKey];
-        });
-    }
+export type InjectOptions = { from?: string; default?: any };
 
-    let args = Array.from(arguments);
-    if (args[0] instanceof Vue) {
-        inject.apply(this, [undefined, ...args]);
-        return;
-    } else {
-        args.length = 1;
-        return function (this: IVuety) {
-            inject.apply(this, [...args, ...Array.from(arguments)]);
-        };
-    }
+export function Inject(target: Vue, propertyKey: string): any;
+export function Inject(dependencyName?: string): DecoratorFactory<string>;
+export function Inject(options: InjectOptions): any;
+export function Inject(this: Vue): DecoratorFactory<string> | undefined {
+	function inject(
+		dependencyNameOrOptions: string | InjectOptions | undefined,
+		target: IVuety,
+		propertyKey: string,
+		descriptor: PropertyDescriptor
+	) {
+		Vuety("Inject", target)(v => {
+			if (!v.options.inject) {
+				v.options.inject = {};
+			}
+			(v.options.inject as {})[propertyKey] = dependencyNameOrOptions || propertyKey;
+			delete v.proto[propertyKey];
+		});
+	}
+
+	let args = Array.from(arguments);
+	if (args[0] instanceof Vue) {
+		inject.apply(this, [undefined, ...args]);
+		return;
+	} else {
+		args.length = 1;
+		return function(this: IVuety) {
+			inject.apply(this, [...args, ...Array.from(arguments)]);
+		};
+	}
 }
